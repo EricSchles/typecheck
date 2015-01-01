3099bde4c7bff56133aabf474b0ed9986ee9f41d
EricSchles <ericschles@gmail.com>
2015-01-01 12:32:14 -0500

    things

--- TypeChecker.java
+++ TypeChecker.java
@@ -17,19 +17,11 @@ class TypeChecker<T>{
 		
 		} else if(generic instanceof Boolean){
 			return "<type Boolean>";
-		} else if(generic instanceof Double)
+		} else if(generic instanceof Double) {
 			return "<type Double>";
 
-		else{
-			String class_name = generic.getClass().toString();
-			try{
-				Class<?> c = Class.forName( class_name );
-				String result = "<types " + c.getFields() + " " + c.getDeclaredFields() + "in "+generic.getClass() +" >";
-				return result;
-			} catch(ClassNotFoundException e){
-				return ""+e.getMessage();
-			}
-			 
+		} else {
+			return "<Unknown type>"; 
 		}
 	}
 
--- main.java
+++ main.java
@@ -1,42 +1,3 @@
-class TypeChecker<T>{
-	
-	public static <T> void print(T input){
-		System.out.println(input);
-	} 
-	
-	public <T> String type(T generic){
-		if(generic instanceof Integer){
-			
-			return "<type Integer>";
-
-		} else if(generic instanceof String){
-			
-			return "<type String>";
-		
-		} else if(generic instanceof Boolean){
-			return "<type Boolean>";
-		} else if(generic instanceof Double)
-			return "<type Double>";
-
-		else{
-			//yay reflection!
-			//String class_name = generic.getClass();
-			try{
-				print(generic);
-				Class<?> c = Class.forName( generic.getClass().toString() );
-				String result = "<types " + c.getFields() + " " + c.getDeclaredFields() + "in "+generic.getClass() +" >";
-				return result;
-			} catch(ClassNotFoundException e){
-				print("here");
-				return ""+e.getMessage();
-			}
-			 
-		}
-	}
-
-}
-
-class hello{ int x; Boolean y;}
 
 public class main{
 	public static <T> void print(T input){
@@ -44,6 +5,15 @@ public class main{
 	}
 	public static void main(String[] args){
 		//TypeChecker t = new TypeChecker();
-		
+		class hello{ int x; Boolean y;}
+		hello x = new hello();
+		try{
+			Class<?> c = Class.forName("hello");
+			print(c.getFields());
+			print(c.getDeclaredFields()[0]);
+		} catch(ClassNotFoundException e){
+			print("dammit");
+		}
+		 		
 	}
 }
